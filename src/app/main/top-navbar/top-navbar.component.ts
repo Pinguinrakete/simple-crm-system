587d508e9ce082f12c61e2c087f94a9842e33ef3
Rico <ekirschke@web.de>
2025-02-27 15:58:40 +0100

    add content top-navbar

--- src/app/main/top-navbar/top-navbar.component.ts
+++ src/app/main/top-navbar/top-navbar.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { BadgeModule } from 'primeng/badge';
+import { OverlayBadgeModule } from 'primeng/overlaybadge';
+import { AvatarModule } from 'primeng/avatar';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-top-navbar',
-  imports: [],
+  imports: [CommonModule, InputGroupModule, InputGroupAddonModule, ButtonModule, AvatarModule, BadgeModule, OverlayBadgeModule],
   templateUrl: './top-navbar.component.html',
   styleUrl: './top-navbar.component.scss'
 })
 export class TopNavbarComponent {
-
+items: string = '';
 }
