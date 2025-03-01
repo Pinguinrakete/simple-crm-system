fd1a4b3afb80a8e14ac8d012935e40f1c3a4576e
Rico <ekirschke@web.de>
2025-03-01 18:27:02 +0100

    splitter panel add overlay customer data

--- src/app/main/menu-content/customer-data/customer-data.component.ts
+++ src/app/main/menu-content/customer-data/customer-data.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { Dialog } from 'primeng/dialog';
+import { SplitterModule } from 'primeng/splitter';
+import { TabViewModule } from 'primeng/tabview';
 
 @Component({
   selector: 'app-customer-data',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [ButtonModule, Dialog, SplitterModule, TabViewModule],
   templateUrl: './customer-data.component.html',
   styleUrl: './customer-data.component.scss'
 })
 export class CostumerDataComponent {
+  visible: boolean = false;
 
+  showAddCostumerData() {
+      this.visible = true;
+  }
 }
 
