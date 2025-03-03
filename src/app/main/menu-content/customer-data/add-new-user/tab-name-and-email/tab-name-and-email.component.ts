a10a521eb1151736dd5562ba2a1d9e8cc12f1466
Rico <ekirschke@web.de>
2025-03-03 21:45:44 +0100

    tab name and email input + picture

--- src/app/main/menu-content/customer-data/add-new-user/tab-name-and-email/tab-name-and-email.component.ts
+++ src/app/main/menu-content/customer-data/add-new-user/tab-name-and-email/tab-name-and-email.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputIconModule } from 'primeng/inputicon';
+import { SelectModule } from 'primeng/select';
+import { TextareaModule } from 'primeng/textarea';
+
+interface City {
+  name: string;
+  code: string;
+}
 
 @Component({
   selector: 'app-tab-name-and-email',
-  imports: [],
+  imports: [ButtonModule, IconFieldModule, InputTextModule, InputIconModule, SelectModule, TextareaModule],
   templateUrl: './tab-name-and-email.component.html',
   styleUrl: './tab-name-and-email.component.scss'
 })
 export class TabNameAndEmailComponent {
+  cities: City[] | undefined;
 
+  selectedCity: City | undefined;
+
+  ngOnInit() {
+      this.cities = [
+          { name: 'New York', code: 'NY' },
+          { name: 'Rome', code: 'RM' },
+          { name: 'London', code: 'LDN' },
+      ];
+  }
 }
--- src/app/main/menu-content/customer-data/customer-data.component.ts
+++ src/app/main/menu-content/customer-data/customer-data.component.ts
@@ -3,6 +3,7 @@ import { ButtonModule } from 'primeng/button';
 import { Dialog } from 'primeng/dialog';
 import { SplitterModule } from 'primeng/splitter';
 import { TabViewModule } from 'primeng/tabview';
+import { TabNameAndEmailComponent } from './add-new-user/tab-name-and-email/tab-name-and-email.component';
 import { TabWorkComponent } from './add-new-user/tab-work/tab-work.component';
 import { TabNotesComponent } from './add-new-user/tab-notes/tab-notes.component';
 import { TabPrivateComponent } from './add-new-user/tab-private/tab-private.component';
@@ -10,7 +11,7 @@ import { TabPrivateComponent } from './add-new-user/tab-private/tab-private.comp
 @Component({
   selector: 'app-customer-data',
   standalone: true,
-  imports: [ButtonModule, Dialog, SplitterModule, TabViewModule, TabWorkComponent,TabNotesComponent, TabPrivateComponent],
+  imports: [ButtonModule, Dialog, SplitterModule, TabViewModule, TabNameAndEmailComponent, TabWorkComponent,TabNotesComponent, TabPrivateComponent],
   templateUrl: './customer-data.component.html',
   styleUrl: './customer-data.component.scss'
 })
