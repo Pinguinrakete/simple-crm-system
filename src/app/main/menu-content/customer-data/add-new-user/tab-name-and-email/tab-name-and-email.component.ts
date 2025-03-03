762b1de688a537f659d3ef953a3475ab86e6e80d
Rico <ekirschke@web.de>
2025-03-03 23:39:25 +0100

    tab name and email textarea + buttons

--- src/app/main/menu-content/customer-data/add-new-user/tab-name-and-email/tab-name-and-email.component.ts
+++ src/app/main/menu-content/customer-data/add-new-user/tab-name-and-email/tab-name-and-email.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputTextModule } from 'primeng/inputtext';
 import { InputIconModule } from 'primeng/inputicon';
 import { SelectModule } from 'primeng/select';
 import { TextareaModule } from 'primeng/textarea';
+import { CheckboxModule } from 'primeng/checkbox';
 
 interface City {
   name: string;
@@ -13,20 +14,34 @@ interface City {
 
 @Component({
   selector: 'app-tab-name-and-email',
-  imports: [ButtonModule, IconFieldModule, InputTextModule, InputIconModule, SelectModule, TextareaModule],
+  imports: [ButtonModule, CheckboxModule, IconFieldModule, InputTextModule, InputIconModule, SelectModule, TextareaModule],
   templateUrl: './tab-name-and-email.component.html',
-  styleUrl: './tab-name-and-email.component.scss'
+  styleUrls: ['./tab-name-and-email.component.scss']
 })
-export class TabNameAndEmailComponent {
+export class TabNameAndEmailComponent implements OnInit {
+  woman: boolean = false;
+  man: boolean = false;
+
   cities: City[] | undefined;
-
   selectedCity: City | undefined;
+  visible: boolean = false;
 
-  ngOnInit() {
-      this.cities = [
-          { name: 'New York', code: 'NY' },
-          { name: 'Rome', code: 'RM' },
-          { name: 'London', code: 'LDN' },
-      ];
+  ngOnInit(): void {
+    this.cities = [
+      { name: 'New York', code: 'NY' },
+      { name: 'Rome', code: 'RM' },
+      { name: 'London', code: 'LDN' },
+    ];
+  }
+
+  showAddCostumerData() {
+      this.visible = true;
+  }
+
+  onCheckboxChange() {
+    if (this.woman || this.man) {
+      this.woman = !this.woman;
+      this.man = !this.man;
+    }
   }
 }
