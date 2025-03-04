a4337c2f8384d148cf58f7ee9231b180d12ac014
Rico <ekirschke@web.de>
2025-03-04 14:40:02 +0100

    add-new-user  tab work  add existing user

--- src/app/main/menu-content/customer-data/add-new-user/tab-work/tab-work.component.ts
+++ src/app/main/menu-content/customer-data/add-new-user/tab-work/tab-work.component.ts
@@ -3,18 +3,34 @@ import { ButtonModule } from 'primeng/button';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputTextModule } from 'primeng/inputtext';
 import { InputIconModule } from 'primeng/inputicon';
+import { SelectModule } from 'primeng/select';
 import { TextareaModule } from 'primeng/textarea';
+
+interface City {
+  name: string;
+  code: string;
+}
 
 @Component({
   selector: 'app-tab-work',
-  imports: [ButtonModule, IconFieldModule, InputTextModule, InputIconModule, TextareaModule],
+  imports: [ButtonModule, IconFieldModule, InputTextModule, InputIconModule, SelectModule, TextareaModule],
   templateUrl: './tab-work.component.html',
   styleUrl: './tab-work.component.scss'
 })
 export class TabWorkComponent {
+  cities: City[] | undefined;
+  selectedCity: City | undefined;
   visible: boolean = false;
 
   showAddCostumerData() {
       this.visible = true;
+  }
+
+  ngOnInit() {
+      this.cities = [
+          { name: 'New York', code: 'NY' },
+          { name: 'Rome', code: 'RM' },
+          { name: 'Paris', code: 'PRS' }
+      ];
   }
 }
