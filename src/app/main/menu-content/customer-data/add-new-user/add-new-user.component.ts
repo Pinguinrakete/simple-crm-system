33cc0b05a2764deec4479ab75c61355b2fb3bec3
Rico <ekirschke@web.de>
2025-03-07 18:34:36 +0100

    first step kommunication parent child form

--- src/app/main/menu-content/customer-data/add-new-user/add-new-user.component.ts
+++ src/app/main/menu-content/customer-data/add-new-user/add-new-user.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CommonModule } from '@angular/common';
 import { Dialog } from 'primeng/dialog';
-import { FormsModule, FormBuilder, FormGroup } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { TabViewModule } from 'primeng/tabview';
 import { TabOverviewComponent } from './tab-overview/tab-overview.component';
 import { TabNameAndEmailComponent } from './tab-name-and-email/tab-name-and-email.component';
@@ -12,43 +11,65 @@ import { TabWorkComponent } from './tab-work/tab-work.component';
 import { TabNotesComponent } from './tab-notes/tab-notes.component';
 import { TabFamilyComponent } from './tab-family/tab-family.component';
 
-import { Customer } from '../../../../interfaces/customer.interface';
-import { Company } from '../../../../interfaces/company.interface';
+import { InputCustomerAndCompany } from '../../../../interfaces/customerandcompany.interface';
 
 import { DataService } from '../../../../firebase-service/dataservice.service'
 
 @Component({
   selector: 'app-add-new-user',
-  imports: [ButtonModule, CommonModule, Dialog, FormsModule, ReactiveFormsModule, TabViewModule, TabOverviewComponent,TabNameAndEmailComponent,TabPrivateComponent, TabWorkComponent, TabFamilyComponent, TabNotesComponent],
+  imports: [ButtonModule, CommonModule, Dialog, ReactiveFormsModule, TabViewModule, TabOverviewComponent, TabNameAndEmailComponent, TabPrivateComponent, TabWorkComponent, TabFamilyComponent, TabNotesComponent],
   templateUrl: './add-new-user.component.html',
   styleUrl: './add-new-user.component.scss'
 })
-export class AddNewUserComponent implements OnInit {
-  company!: FormGroup;
+export class AddNewUserComponent {
+  customerandcompany: FormGroup;
 
   constructor(private fb: FormBuilder, private dataService: DataService) { }
 
-  ngOnInit(): void {
-    this.company = this.fb.group({
-      street: '',
-      place: '',
-      federalState: '',
-      zipCode: '',
-      state: '',
-      company: '',
-      position: '',
-      department: '',
-      office: '',
-      phone: '',
-      mobilePhone: '',
-      fax: '',
-      website: ''
+
+    this.customerandcompany = this.fb.group<InputCustomerAndCompany>({
+      customerFirstName: '',
+      customerSecondName: '',
+      customerSurname: '',
+      customerPersonalTitle: '',
+      customerNickname: '',
+      customerMan: true,
+      customerWoman: false,
+      customerEmail: '',
+      customerTextarea: '',
+      customerStreet: '',
+      customerPlace: '',
+      customerFederalState: '',
+      customerZipCode: NaN,
+      customerState: '',
+      customerPhone: NaN,
+      customerMobilePhone: NaN,
+      customerFax: NaN,
+      customerWebsite: '',
+      customerBirthday: NaN,
+      customerAnniversary: NaN,
+      customerPartner: '',
+      customerChildren: '',
+      customerNotes: '',
+      companyStreet: '',
+      companyPlace: '',
+      companyFederalState: '',
+      companyZipCode: NaN,
+      companyState: '',
+      companyCompany: '',
+      companyPosition: '',
+      companyDepartment: '',
+      companyOffice: '',
+      companyPhone: NaN,
+      companyMobilePhone: NaN,
+      companyFax: NaN,
+      companyWebsite: '',
     });
   }
 
 saveTabCompanyData() {
-    console.log(this.company.value);
-    console.log("ZipCode: ", this.company.get('zipCode')?.value);
+    console.log(this.customerandcompany.value);
+    console.log("ZipCode: ", this.customerandcompany.get('zipCode')?.value);
     // const customerData = this.company.value;
     // this.dataService.saveCompanyData(customerData); // Speichern in Firebase
 }
